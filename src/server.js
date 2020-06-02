fa3aa26ca4ce18192d290ddc375243579a3a1984
clovisnunes <clobnjuh@hotmail.com>
2020-06-02 01:37:07 -0300

    node + nunjucks

---
+++ src/server.js
@@ -0,0 +1,32 @@
+var nunjucks = require('nunjucks');
+
+var express = require('express');
+
+var app = express();
+
+const data = require('../data');
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    const rechome = data.slice(0,6);
+
+    res.render('index.njk', {data: rechome});
+});
+
+app.get('/sobre', function (req, res) {
+    res.render('pages/sobre/index.njk');
+});
+
+app.get('/receitas', function (req, res) {
+    res.render('pages/receitas/index.njk', {data: data});
+});
+
+app.listen(3000, function () {
+    console.log('App listening on port 3000!');
+  });
